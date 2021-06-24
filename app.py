e99a001fe4588b151ecdb654b11d11efca20bba4
Hazel <hazel@edibly.ca>
2021-06-24 14:04:54 -0700

    add image scraping

--- app.py
+++ app.py
@@ -7,33 +7,57 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from requests_html import HTMLSession
 from selenium.webdriver.chrome.options import Options
 import os
+import io
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import requests
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import storage
+import uuid
 
 
-def bruteForceCleanTextLol(text):
-    try:
-        text1 = text.string.lower()
-    except:
-        text1 = text.lower()
-    text = " ".join(text.split(","))
-    text = " ".join(text.split('.'))
-    text = " ".join(text.split('('))
-    text = " ".join(text.split(')'))
-    text = " ".join(text.split('"'))
-    text = " ".join(text.split('-'))
-    text = " ".join(text.split(':')).lower()
+def replaceTextBetween(originalText, replacementText):
+    # 😤this is the no regex zone🙅‍♀️
+    # all my homies hate regex 👎💥
+    delimiterA = "width="
+    delimiterB = ",format"
 
-# def veganCatcher(text):
-#     text = bruteForceCleanTextLol(text)
-#     vegan = re.search(
-#         r'\bvegan\b', text)
-#     if vegan:
-#         return True
+    # eg https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1920,format=auto,quality=50/https://cdn.doordash.com/media/photos/81b5393f-36d6-4c84-a9de-ed7afcf8e301-retina-large.jpg 1920w
+    # looking for this, change width=1920 to width=400
+    leadingText = originalText.split(delimiterA)[0]
+    trailingText = originalText.split(delimiterB)[1]
+    newString = leadingText + delimiterA + \
+        replacementText + delimiterB + trailingText
+    return newString
+
+
+def handleImage(doordashImgUrl, ):
+    # this is SO STUPID
+    # first get compressed version of image
+    id = uuid.uuid4()
+
+    reducedImgSize = "400"
+    smallerDoordashImgUrl = replaceTextBetween(doordashImgUrl, reducedImgSize)
+    response = requests.get(smallerDoordashImgUrl)
+
+    # Upload it to firebase
+    bucket = storage.bucket("menu-buddy-9c09c.appspot.com")
+    blob = bucket.blob('images/${id}.jpg')
+    # should i make separate buckets for each restaurant
+    blob.upload_from_string(response.content, content_type='image/jpeg')
+
+    # then get and return its new image url
+    blob.make_public()
+    return blob.public_url
 
 
 def scrape_from_url(url):
-    print("scraping...")
+
+    cred = credentials.Certificate(
+        './menu-buddy-9c09c-firebase-adminsdk-x7p8i-37b112465c.json')
+    app = firebase_admin.initialize_app(cred)
+# comment me out for home use
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
@@ -43,7 +67,8 @@ def scrape_from_url(url):
         executable_path=os.environ.get("CHROMEDRIVER_PATH"),
         options=chrome_options
     )
-
+    # Uncomment me out:
+    # browser = webdriver.Firefox()
     session = HTMLSession()
     page = browser.get(url)
     wait(browser, 10).until(
@@ -90,6 +115,10 @@ def scrape_from_url(url):
                 dish["price"] = t[3].text
             except:
                 pass
+            try:
+                img = button.findAll("img")
+                if len(img) > 0:
+                    dish["imageUrl"] = handleImage(img[0]['srcset'].split()[0])
         if catName not in categories:
             categories[catName] = {"name": catName,
                                    "description": catDesc, "dishes": [dish]}
@@ -102,10 +131,14 @@ def scrape_from_url(url):
     browser.close()
     return(categories)
 
+# uncomment me
+# scrape_from_url(
+#     "https://www.doordash.com/store/red-robin-gourmet-burgers-bc-victoria-954678")
 
+
+# Comment me out
 app = Flask(__name__)
 CORS(app)
-# if __name__ == "__main__":
 
 
 @app.route("/<incompleteUrl>", methods=["GET"])
@@ -113,5 +146,3 @@ def render(incompleteUrl):
     url = "https://www.doordash.com/store/" + incompleteUrl
     categories = scrape_from_url(url)
     return jsonify(categories), 201
-
-    # return (render_template("index.html", title=title))
